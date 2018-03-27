c3a2f0474faf3452893fa028b65976ff407283c9
Ray Litchi <raysworld@outlook.com>
2018-03-27 09:41:02 +0800

    change model file path

--- python/run_sim.bat
+++ python/run_sim.bat
@@ -2,8 +2,8 @@
 setlocal enabledelayedexpansion
 
 set IPADDR="127.0.0.1"
-set PEG_PATH="%cd%/../model_files/tri_peg_hole/peg_tri_r_200_h_801.stl"
-set HOL_PATH="%cd%/../model_files/tri_peg_hole/hol_tri_r_201_h_901.stl"
+set PEG_PATH="%cd%\..\models\tri_peg_hole\peg_tri_r_200_h_801.stl"
+set HOL_PATH="%cd%\..\models\tri_peg_hole\hol_tri_r_201_h_901.stl"
 set X_START=-0.04
 set X_END=0.04
 set Y_START=-0.04
@@ -18,5 +18,5 @@ set COUNT=%1
 for /l %%i in (1, 1, %COUNT%) do (
 set /a PORT=%PORT_START%+%%i-1
 set /a ANGLE=5*%%i-5
-start python run_sim.py %PEG_PATH% %HOL_PATH% 0.0 15.0 !ANGLE! --ip-addr %IPADDR% --port !PORT! --precision-x %X_PRECISION% --precision-y %Y_PRECISION% --plot --x-start %X_START% --x-end %X_END% --y-start %Y_START% --y-end %Y_END%
+start python run_sim.py %PEG_PATH% %HOL_PATH% 0.0 20.0 !ANGLE! --ip-addr %IPADDR% --port !PORT! --precision-x %X_PRECISION% --precision-y %Y_PRECISION% --plot --x-start %X_START% --x-end %X_END% --y-start %Y_START% --y-end %Y_END%
 )
