c67729698ebb9bdaf67c21258ba967d186163a23
Rui LI <raysworld@outlook.com>
2021-02-22 11:50:52 +0800

    find the only local minimum

--- python/post_process.py
+++ python/post_process.py
@@ -35,6 +35,49 @@ def csv2array(file_path):
 
     return x, y, z
 
+def csv2outname(file_path):
+    '''
+    Input load file path, output a output name string
+
+    e.g. Input:  ../results/result_001.csv
+         Output: ../resutls/result_001.png
+    '''
+    return file_path.replace('.csv', '.png')
+
+
+def local_minima2(mat, x_label, y_label):
+    '''
+    Input 2d matrix, return the only one local minimum which 
+    should be the bottom of the ARIE
+    '''
+    coordinates = peak_local_max(-mat, min_distance=20)
+
+    coord_new = coordinates
+
+    x = x_label[coord_new[:,1]]
+    y = y_label[coord_new[:,0]]
+    d = [mat[x[0],x[1]] for x in coord_new]
+
+    # find the only one local minia that could be the bottom of the ARIE
+    # rule: the point that is closest to the origin
+    dist = np.sqrt(np.square(x) + np.square(y))
+    min_idx = np.argmin(dist)
+
+    return coord_new[:,1], coord_new[:,0], min_idx, [x, y, d] # x_indexes, y_indexes, min_idx, [x,y,d]
+
+def plot_local_minima(mat, x_idxs, y_idxs, min_idx, save_path_and_name):
+    fig, ax = plt.subplots()
+
+    ax.imshow(mat, cmap=plt.cm.gray)
+    ax.autoscale(False)
+    ax.plot(x_idxs, y_idxs,'r.')
+    ax.plot(x_idxs[min_idx], y_idxs[min_idx],'b*')
+    ax.axis('off')
+    ax.set_title('Peak local min')
+
+    plt.savefig(save_path_and_name)
+
+
 def local_minima(mat, x_label, y_label):
     coordinates = peak_local_max(-mat, min_distance=20)
 
@@ -87,12 +130,12 @@ def draw_ARIE(x, y, z, plot_mode = 0, slice=1):
 
         surf = ax.plot_surface(X, Y, Z, cmap=parula(), antialiased=True)
 
-        zz = np.sort(np.unique(z))
-        zz_range = np.linspace(zz[0], zz[-1], 4)
-        ax.set_zticks(zz_range)
-        ax.set_zlim((zz[0], zz[-1]))
+        # zz = np.sort(np.unique(z))
+        # zz_range = np.linspace(zz[0], zz[-1], 4)
+        # ax.set_zticks(zz_range)
+        # ax.set_zlim((zz[0], zz[-1]))
 
-        ax.zaxis.set_major_formatter(ticker.StrMethodFormatter("{x:.3f}"))
+        # ax.zaxis.set_major_formatter(ticker.StrMethodFormatter("{x:.3f}"))
     else: # plot_mode == 1
         print('len(x)={}\nlen(y)={}\nlen(z)={}'.format(len(x),len(y),len(z)))
         points = ax.scatter(x[::slice], y[::slice], z[::slice], c=z[::slice], cmap=cm.coolwarm, marker='.')
@@ -195,7 +238,7 @@ if __name__ == "__main__":
     
     x, y, z = csv2array(file_path)
 
-    draw_ARIE(x, y, z, plot_mode=plot_mode, slice=slice)
+    # draw_ARIE(x, y, z, plot_mode=plot_mode, slice=slice)
 
     x_unique = np.unique(x)
     y_unique = np.unique(y)
@@ -207,18 +250,9 @@ if __name__ == "__main__":
             Z[j, i] = z[k]
             k = k+1
     
-    xx, yy, dd = local_minima(Z, x_unique, y_unique)
-    dd = np.array(dd)
+    x_indexes, y_indexes, min_idx, xyd = local_minima2(Z, x_unique, y_unique)
 
-    idxx = np.logical_and(xx>=-0.01, xx<=0.01)
-    xxx = xx[idxx]
-    yyy = yy[idxx]
-    ddd = dd[idxx]
-
-    R = np.array([xxx, yyy, ddd])
-    with np.printoptions(precision=4, suppress=True):
-        print(R)
-
-    plt.show()
+    output_fig_path = csv2outname(file_path)
+    plot_local_minima(Z, x_indexes, y_indexes, min_idx, output_fig_path)
 
 
